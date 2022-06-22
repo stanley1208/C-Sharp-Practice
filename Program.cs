0265be13cfb7f081045c6dcccb4a4abe1dff7771
stanley1208 <54892792+stanley1208@users.noreply.github.com>
2022-06-22 19:11:58 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace IntegerDataTypes
+{
+    /// <summary>
+    /// Integer Data Types lecture code
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Demonstrates integer data types
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            int totalSecondsPlayed = 100;
+            const int SecondsPerMinutes = 60;
+            // Exercixe3
+            int age = 21;
+
+
+            // calculate minutes and seconds played
+            int minutesPlayed = totalSecondsPlayed / SecondsPerMinutes;
+            int secondsPlayed = totalSecondsPlayed % SecondsPerMinutes;
+
+            // print minutes and seconds played
+            Console.WriteLine("Minutes Played: " + minutesPlayed);
+            Console.WriteLine("Seconds Played: " + secondsPlayed);
+
+            // Exercixe3
+            Console.WriteLine("My age is " + age);
+
+            // Exercixe4
+            Console.Write("Enter first altitude: ");
+            int firstAltitude = int.Parse(Console.ReadLine());
+            Console.Write("Enter second altitude: ");
+            int secondAltitude = int.Parse(Console.ReadLine());
+            int changealtitude = secondAltitude - firstAltitude;
+            Console.WriteLine("The change of altitude is " + changealtitude);
+
+        }
+    }
+}
