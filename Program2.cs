078ffa46d8c43e19e52a0cf16e52525517b5bc1e
stanley1208 <54892792+stanley1208@users.noreply.github.com>
2022-06-27 12:53:05 +0800

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FloatingPointDataTypes
+{
+    /// <summary>
+    /// Floating Point Data Types lecture code
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Demostrates floating point data types
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int score = 1360;
+            int totalSecondsPlayed = 10000;
+
+
+            // calculate and print points per seconds
+            float pointsPerSecond=(float)score/totalSecondsPlayed;
+            Console.WriteLine("Point per second: " + pointsPerSecond);
+        }
+    }
+}
