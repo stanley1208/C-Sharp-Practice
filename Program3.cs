edf26d5007e06d7e129337e5af50bced3fa41c17
stanley1208 <54892792+stanley1208@users.noreply.github.com>
2022-06-30 18:46:25 +0800

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReadingDocumentation
+{
+    /// <summary>
+    /// Reading Documentation lecture code
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Demonstrates reading documentation
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // calculate and print cosine of 45 degrees
+            float cosine = MathF.Cos(45*MathF.PI/180);
+            Console.WriteLine("Cosine of 45 degrees: " + cosine);
+        }
+    }
+}
