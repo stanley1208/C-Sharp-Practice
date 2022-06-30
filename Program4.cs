2c561d0bc4eb1116042fc091df1ec0360079c673
stanley1208 <54892792+stanley1208@users.noreply.github.com>
2022-06-30 18:58:46 +0800

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Trigonometry_with_floats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // calculate and print cosine of 45 degrees
+            Console.Write("Enter the degree: ");
+            int degrees = int.Parse(Console.ReadLine());
+            float cosine = MathF.Cos(degrees * MathF.PI / 180);
+            float sine = MathF.Sin(degrees * MathF.PI / 180);
+            Console.WriteLine("Cosine of " + degrees + " degrees: " + cosine);
+            Console.WriteLine("Sine of " + degrees + " degrees: " + sine);
+        }
+    }
+}
