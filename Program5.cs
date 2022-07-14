f1c30ff1b5d249dba55590a11d5e7fdd046dbfa2
stanley1208 <54892792+stanley1208@users.noreply.github.com>
2022-07-14 18:22:20 +0800

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Debugging_a_Console_App
+{
+    internal class Program5
+    {
+        static void Main(string[] args)
+        {
+            float angle;
+
+            // prompt for and get angle
+            Console.Write("Enter angle in degrees: ");
+            angle=float.Parse(Console.ReadLine());
+
+            // convert angle to radians
+            angle*=MathF.PI/180;
+
+            // calculate and print cos and sin
+            Console.WriteLine(MathF.Cos(angle));
+            Console.WriteLine(MathF.Sin(angle));
+        }
+    }
+}
