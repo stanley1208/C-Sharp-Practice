ac9f9588a591683990b11bd44a35b39e67436f9c
stanley1208 <54892792+stanley1208@users.noreply.github.com>
2022-07-26 19:25:23 +0800

    Add files via upload

---
+++ Program7.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise8
+{
+    /// <summary>
+    /// Exercise 8 solution
+    /// </summary>
+    internal class Program7
+    {
+        /// <summary>
+        /// Demonstrates rolling and using two dice
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            // initialize random number generator
+            RandomNumberGenerator.Initialize();
+
+            // create two dice
+            Die die1=new Die(6);
+            Die die2=new Die(6);
+
+            // tell the dice to roll themselves
+            die1.Roll();
+            die2.Roll();
+            // print the top sides and the sum of the dice
+            Console.WriteLine(die1.TopSide);
+            Console.WriteLine(die2.TopSide);
+            int sum = die1.TopSide + die2.TopSide;
+            Console.WriteLine(sum);
+        }
+    }
+}
